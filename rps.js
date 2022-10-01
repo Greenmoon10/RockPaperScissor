958983c4cf702c96d8f27f63a4fb64e631b902e7
Yordy <yordyracing2610@gmail.com>
2022-10-01 12:43:45 -0400

    computer selection dont change between rounds

--- rps.js
+++ rps.js
@@ -10,7 +10,7 @@ function getComputerChoice()
   {
     eleccioncompu = 'Paper';
   }
-  else
+  else 
   {
     eleccioncompu = 'Scissor';
   }
@@ -19,72 +19,98 @@ function getComputerChoice()
 
 function playRound(playerSelection, computerSelection)
 {
-  
+  const container = document.querySelector('#container');
+  container.setAttribute('style','border:3px solid black; background:pink; max-width:1100px; margin: 0 auto; padding: 0 30px;');
   if(playerSelection == 'rock' && computerSelection == 'Paper')
   {
-    console.log('You lose! , Paper beats rocks')
+    const resultado = document.createElement('div');
+    resultado.textContent = 'You lose! , Paper beats rock!';
+    container.appendChild(resultado);
     return 1;
   }
 
   else if(playerSelection == 'rock' && computerSelection == 'Scissor')
   {
-    console.log(`You win! , Rock beats scissor`)
+    const resultado = document.createElement('div');
+    resultado.textContent = 'You win! , Rock beats scissor';
+    container.appendChild(resultado);
     return 2;
   }
 
   else if(playerSelection == 'rock' && computerSelection == 'Rock')
   {
-    console.log(`Is a tie (Rock tie)`);
+    const resultado = document.createElement('div');
+    resultado.textContent = 'Is a tie (Rock tie)';
+    container.appendChild(resultado);
     return 0;
   }
 
   else if(playerSelection == 'paper' && computerSelection == 'Rock')
   {
-    console.log('You win! , Paper beats Rock');
+    const resultado = document.createElement('div');
+    resultado.textContent = 'You win!, Paper beats Rock';
+    container.appendChild(resultado);
     return 2;
   }
 
   else if(playerSelection == 'paper' && computerSelection == 'Scissor')
-  {
-    console.log('You lose! , Scissor beats Paper');
+  { 
+    const resultado = document.createElement('div');
+    resultado.textContent = 'You lose!, Scissor beats Paper';
+    container.appendChild(resultado);
     return 1;
   }
 
   else if(playerSelection == 'paper' && computerSelection == 'Paper')
-  {
-    console.log('Is a tie (Paper tie)');
+  { 
+    const resultado = document.createElement('div');
+    resultado.textContent = 'Is a tie (Paper tie)';
+    container.appendChild(resultado);
     return 0;
   }
 
   else if(playerSelection == 'scissor' && computerSelection == 'Paper')
   {
-    console.log('You win! , Scissor beats Paper');
+    const resultado = document.createElement('div');
+    resultado.textContent = 'You win!, Scissor beats Paper';
+    container.appendChild(resultado);
     return 2;
   }
 
   else if(playerSelection == 'scissor' && computerSelection == 'Rock')
   {
-    console.log('You lose! , Rock beats Scissor');
+    const resultado = document.createElement('div');
+    resultado.textContent = 'You lose!, Rock beats Scissor';
+    container.appendChild(resultado);
     return 1;
   }
 
-  else
+  else if(playerSelection == 'scissor' && computerSelection == 'Scissor')
   {
-    console.log('Is a tie (Scissor tie)');
+    const resultado = document.createElement('div');
+    resultado.textContent = 'Is a tie (Scissor tie)';
+    container.appendChild(resultado);
     return 0;
   }
 
 }
-
-
 let contadorPlayer = 0;
 let contadorComputadora = 0;
-for(let i = 0 ; i<15 ; i++)
-{
 
-  let playerSelection = prompt('Type Rock , Paper or Scissor');
-  playerSelection = playerSelection.toLowerCase();
+for(let i = 1 ; i <= 5 ; i++)
+{
+  let playerSelection;
   const computerSelection = getComputerChoice();
+
+  const btnrock = document.querySelector('#rock');
+  btnrock.onclick = () => playRound(playerSelection = 'rock', computerSelection);
+
+  const btnpaper = document.querySelector('#paper');
+  btnpaper.onclick = () => playRound(playerSelection = 'paper', computerSelection);
+
+  const btnscissor = document.querySelector('#scissor');
+  btnscissor.onclick = () => playRound(playerSelection = 'scissor', computerSelection);
+  
 
   
   if(playRound(playerSelection, computerSelection) == 1)
@@ -95,22 +121,9 @@ for(let i = 0 ; i<15 ; i++)
   {
   contadorPlayer++;
   }
+ console.log(contadorComputadora);
+}
 
-  if(contadorComputadora == 5 || contadorPlayer == 5)
-  break;
-  
-}
-console.log(`Conteo del jugador ${contadorPlayer}`);
-console.log(`Conteo de la computadora ${contadorComputadora}`);
-
-if(contadorPlayer > contadorComputadora)
-{
-  alert("Haz ganado!");
-}
-else
-{
-  alert("Haz perdido!");
-}
 
 /*TIE == 0 ////// LOSE == 1 ////// WIN == 2*/
 
